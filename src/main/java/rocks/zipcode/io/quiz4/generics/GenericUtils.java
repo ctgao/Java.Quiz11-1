ed5534ec3c1b5e555677d8ed64a20880ab562704
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-09-11 14:36:15 -0400

    it doesn't work but saving progress

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -1,15 +1,21 @@
 package rocks.zipcode.io.quiz4.generics;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class GenericUtils {
     public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(Set<_> originalSet) {
-        return null;
+        List<_> originalList = new ArrayList<>(originalSet);
+        Group<Group<_>> result = new Group<>();
+        for(int setSize = originalList.size(); setSize < originalList.size(); setSize++){
+            Group<_> innerGroup = new Group<>();
+            for(int i = 0; i < setSize; i++){
+
+            }
+        }
+        return result;
     }
 
     public static <_ extends Comparable> Iterable<? extends Iterable<_>> powerSet(_... originalSet) {
